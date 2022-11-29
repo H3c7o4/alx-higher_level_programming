009be193bfc52c4fa594f129161670ba8a1ded2c
H3c7o4 <hectorvladitok@gmail.com>
2022-11-29 12:41:29 +0100

    Adding a file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 8):
     for j in range(0, 10):
         if i < j:
-            print(f'{i}{j},', end=" ")
+            print('{}{},'.format(i, j), end=" ")
         else:
             continue
 print(89)
