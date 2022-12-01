a9e51645736ba312bcdef95d59cd6382431c150b
H3c7o4 <hectorvladitok@gmail.com>
2022-12-01 12:09:13 +0100

    Adding a new file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv) - 1
-    if l == 0:
-        print('{:d} arguments.'.format(l))
-    elif l == 1:
-        print('{:d} argument:'.format(l))
-        print('{:d}: {:s}'.format(argv))
+    nargs = len(sys.argv) - 1
+    if (nargs == 0):
+        print("{:d} arguments.".format(nargs))
+    elif (nargs == 1):
+        print("{:d} argument:".format(nargs))
     else:
-        print('{:d} arguments:'.format(l))
-        for i in range(l + 1):
-            if i == 0:
-                continue
-            print('{:d}: {:s}'.format(i, sys.argv[i]))
+        print("{:d} arguments:".format(nargs))
+    for i in range(len(sys.argv)):
+        if (i == 0):
+            continue
+        print("{:d}: {:s}".format(i, sys.argv[i]))
