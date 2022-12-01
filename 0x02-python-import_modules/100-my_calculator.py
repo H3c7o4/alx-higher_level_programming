b40979dfc090f3f191c0cb560334cd2e69aa7ca2
H3c7o4 <hectorvladitok@gmail.com>
2022-12-01 13:07:56 +0100

    Adding a new file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,22 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     argc = len(sys.argv)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if argc != 4:
-        print(r'Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
-    if argc == 4:
-        if sys.argv[2] == '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif sys.argv[2] == '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif sys.argv[2] == '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-        elif sys.argv[2] == '/':
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
+    for str in range(argc):
+        if (argc != 4):
+            print("Usage: {:s} <a> <operator> <b>".format(sys.argv[str]))
             exit(1)
-        exit(0)
+        else:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            if (sys.argv[2] == '+'):
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            elif (sys.argv[2] == '-'):
+                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            elif (sys.argv[2] == '*'):
+                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            elif (sys.argv[2] == '/'):
+                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
+            exit(0)
