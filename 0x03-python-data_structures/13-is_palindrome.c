544d6991eedebe6e43228fb80a3ffde295d8dbb0
H3c7o4 <hectorvladitok@gmail.com>
2022-12-05 10:02:15 +0100

    Adding a new file

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,37 @@
 #include "lists.h"
+/**
+ * PalindromeRecur - Check if linked list is palindrome.
+ * @left: Head of list
+ * @right: Last element take it in recursion.
+ */
+int PalindromeRecur(listint_t **left, listint_t *right)
+{
+	int pal;
 
+	if (right == NULL)
+		return (1);
+
+	pal = PalindromeRecur(left, right->next);
+	if (pal == 0)
+		return (0);
+
+	pal = (right->n == (*left)->n);
+
+	*left = (*left)->next;
+	return (pal);
+}
+
+/**
+ * is_palindrome - Check if linked list is palindrome.
+ * @head: give list
+ * @right: Last element take it in recursion.
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current, *temp1, *temp2;
-    int first_elem, second_elem;
+	int res;
 
-    if (*head == NULL || (*head)->next == NULL || (*head)->n == (*head)->next->n)
-        return (1);
-
-    current = *head;
-    first_elem = (*head)->n;
-    while (current->next->next != NULL)
-        current = current->next;
-
-    second_elem = current->next->n;
-    if (first_elem == second_elem)
-    {
-        temp1 = *head;
-        *head = (*head)->next;
-        temp2 = current->next;
-        current->next = NULL;
-        free(temp1);
-        free(temp2);
-        return (is_palindrome(head));
-    }
-    else
-        return (0);
+	if (!head)
+		return (0);
+	res = PalindromeRecur(head, *head);
+	return (res);
 }
