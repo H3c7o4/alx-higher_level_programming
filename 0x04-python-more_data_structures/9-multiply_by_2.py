fea439d31e74036e885a2e1cadc1d5afab134c3e
H3c7o4 <hectorvladitok@gmail.com>
2022-12-07 12:22:46 +0100

    Adding a new file

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = {keys: x * 2 for keys in a_dictionary.keys() for x in a_dictionary.get(keys)}
+    new_dic = {keys: x * 2 for(keys, value) in a_dictionary.items()}
     return new_dic
