1c4aa0f3f70e188ad9b2b7c163fde53342789b5b
H3c7o4 <hectorvladitok@gmail.com>
2022-12-20 13:49:13 +0100

    Adding a new file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Square moule. This module contains a class that defines a square"""
+"""Square module.
+
+This module contains a class that defines a square.
+
+"""
+
 
 class Square():
-    """Defines a square"""
+    """Defines a square."""
 
     pass
