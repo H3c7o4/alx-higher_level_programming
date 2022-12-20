0f187964d09378417948b519d82ed6a25229f218
H3c7o4 <hectorvladitok@gmail.com>
2022-12-20 14:53:55 +0100

    Adding a new file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,6 +7,16 @@ Square class: defines a square
 class Square:
     """class Square that defines a squaare"""
     def __init__(self, size):
+        """Initialize attributes"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of the square"""
+        return (self.__size)
+    
+    @property.setter
+    def size(self, value):
         """Sets the necessary attributes for the Square object.
         Args:
             size (int): the size of one edge of the square.
@@ -14,20 +24,14 @@ class Square:
             TypeError: if size is not given as an integer.
             ValueError: if size is less than 0.
         """
-        if type(size) is int:
-            if size >= 0:
-                self.__size = size
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
             else:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
 
-    @property
-    def area(self):
-        """Return the area of the square"""
-        return (self.__size * self.__size)
-    
-    @property.setter
-    def size(self, value):
-        """Set the value of size"""
-        self.__size = value
+        def area(self):
+            """Return the area of the square"""
+            return (self.__size * self.__size)
