36f43c81a8945df19b62426ec9c480c0a3a10154
H3c7o4 <hectorvladitok@gmail.com>
2023-01-04 16:44:16 +0100

    Adding a new file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
     if type(a) in [int, float]:
         try:
             a = int(a)
-        except TypeError:
+        except ValueError:
             print('a must be an integer')
     else:
         raise TypeError('a must be an integer')
@@ -25,7 +25,7 @@ def add_integer(a, b=98):
     if type(b) in [int, float]:
         try:
             b = int(b)
-        except TypeError:
+        except ValueError:
             print('b must be an integer')
     else:
         raise TypeError('b must be an integer')
