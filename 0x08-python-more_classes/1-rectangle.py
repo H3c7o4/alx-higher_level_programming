96258fcdf8ae710b6894f2a7e1bcb9341ffad93f
H3c7o4 <hectorvladitok@gmail.com>
2023-01-04 10:23:23 +0100

    Adding a new file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,12 +25,12 @@ class Rectangle:
             TypeError: if width is not given as an integer.
             ValueError: if width is less than 0.
         """
-        if (value is not int):
+        if (type(value) !=  int):
             raise TypeError("width must be an integer")
         elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
-            return self.__width = value
+            self.__width = value
 
     @property
     def height(self):
@@ -46,9 +46,9 @@ class Rectangle:
             TypeError: if width is not given as an integer.
             ValueError: if height is less than 0.
         """
-        if (value is not int):
+        if (type(value) !=  int):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
-            return (self.__height = value)
+            self.__height = value
