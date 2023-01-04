0d4bdac04543b0233fe50df06177fd05d7aa1892
H3c7o4 <hectorvladitok@gmail.com>
2023-01-04 10:45:29 +0100

    Adding a new file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
-            return self.__width = value
+            self.__width = value
 
     @property
     def height(self):
@@ -51,7 +51,7 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
-            return (self.__height = value)
+            self.__height = value
 
     def area(self):
         """Retrieves the area of the Rectangle"""
