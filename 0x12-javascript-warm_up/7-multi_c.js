536341411f835b06d2cee36eb3d189f29e6fc918
H3c7o4 <hectorvladitok@gmail.com>
2023-03-13 15:14:11 +0100

    Adding a new file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,10 +6,7 @@ let x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else if (x < 0) {
-  return;
-} else {
-  while (x) {
-    console.log(word);
-    x--;
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
