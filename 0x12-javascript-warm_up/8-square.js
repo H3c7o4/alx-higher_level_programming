b7bb998a086fc48d728242dd7f2252d0d49924e0
H3c7o4 <hectorvladitok@gmail.com>
2023-03-13 15:29:53 +0100

    Adding a new file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i > process.argv[2]; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
   }
 }
