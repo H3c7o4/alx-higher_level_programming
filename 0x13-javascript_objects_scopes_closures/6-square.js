eb1efb9bfd0653c8f19dd35569f44f8e67460f43
H3c7o4 <hectorvladitok@gmail.com>
2023-03-14 14:07:31 +0100

    Adding a new file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,13 +5,13 @@ class Square extends require('./5-square.js') {
     if (!c) {
       c = 'x';
     }
-    
+
     const charArray = [];
-    
+
     for (let i = 0; i < this.width; i++) {
       charArray.push(c.repeat(this.width));
     }
-    
+
     console.log(charArray.join('\n'));
   }
 }
