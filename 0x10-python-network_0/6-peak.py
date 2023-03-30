cef6feb489240b2b5f3e5cc3bf4002297ee9abde
H3c7o4 <hectorvladitok@gmail.com>
2023-03-30 16:54:54 +0200

    Adding the file 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,21 +11,27 @@ def find_peak(list_of_integers):
          >> 7
     """
 
-    if list_of_integers == []:
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
-    length_of_list = len(list_of_integers)
-    if length_of_list == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
-    if length_of_list == 2:
-        return max(list_of_integers)
 
-    half_of_length = int(length_of_list / 2)
-    peak = list_of_integers[half_of_length]
-    if peak > list_of_integers[half_of_length - 1] \
-            and peak > list_of_integers[half_of_length + 1]:
-        return peak
-    elif peak > list_of_integers[half_of_length + 1]:
-        return find_peak(list_of_integers[:half_of_length])
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
     else:
-        return find_peak(list_of_integers[half_of_length + 1:])
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
